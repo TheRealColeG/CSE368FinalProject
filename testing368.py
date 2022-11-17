9b716692579a7b6ab16c65267368043756af354b
TheRealColeG <98725167+TheRealColeG@users.noreply.github.com>
2022-11-17 11:36:38 -0500

    Update testing368.py

--- testing368.py
+++ testing368.py
@@ -1,5 +1,7 @@
 import math
 import numpy as np
+import pandas as pd
+from sklearn.linear_model import LinearRegression
 
 class User:
 
@@ -42,7 +44,7 @@ class Rating:
 def initializeMovies():
 
     MovieBase = {} #List of Movie objects
-    with open("u.item") as file:
+    with open("CSE368FinalProject/u.item") as file:
         allLines = file.readlines()
         for line in allLines:
 
@@ -60,7 +62,7 @@ def initializeMovies():
 def initializeUsers():
 
     UserBase = {} #List of User objects
-    with open("u.user") as file:
+    with open("CSE368FinalProject/u.user") as file:
         #May have to .strip() for \n
         allLines = file.readlines()
         for line in allLines:
@@ -81,7 +83,7 @@ def initializeRatings(movieBase, userBase):
     #In reality, no actual ratingBase is needed since the values will be added to the user/movie bases but just in case they are I'm going to make a base for them
     #Let's be real, is anyone strapped for memory in such a low level application such as this?
     RatingBase = []
-    with open("u.data") as file:
+    with open("CSE368FinalProject/u.data") as file:
         allLines = file.readlines()
         for line in allLines:
 
@@ -99,22 +101,14 @@ def initializeRatings(movieBase, userBase):
     
     return RatingBase
 
-def getUsers():
-    with open("u.user") as file:
-        allLines = file.readlines()
-        for line in allLines:
-            fields = line.split("|")
-            print("ID:",fields[0],"AGE:",fields[1],"GENDER:",fields[2],"OCCUPATION:",fields[3],"ZIP:",fields[4])
 
 if __name__ == "__main__":
     movieBase = initializeMovies()
-    for movie in movieBase.keys():
-        print(movieBase[movie].genres)
     
     userBase = initializeUsers()
-    for user in userBase.keys():
-        print(userBase[user].zipcode)
 
     ratingBase = initializeRatings(movieBase, userBase)
-    print(ratingBase)
+
+    ratings = pd.read_csv("CSE368FinalProject/u.data", delimiter="\t")
+    users = pd.read_csv("CSE368FinalProject/u.user",delimiter="|")
 
